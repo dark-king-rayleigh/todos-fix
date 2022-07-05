2f5577f4cafafe58fd8275b9ce65845386c7a09e
dark-king-rayleigh <ayushbaral17@gmail.com>
2022-07-05 17:21:09 +0545

    feat(todo):show error state on add form

--- src/components/todo-form/todo-form.jsx
+++ src/components/todo-form/todo-form.jsx
@@ -5,10 +5,17 @@ import './todo-form.scss';
 export const TodoForm = () => {
   const { todos, setTodos } = React.useContext(TodosContext);
   const [task, setTask] = React.useState('');
+  const [error, setError] = React.useState('');
 
   const handleAddTodo = () => {
     // Fin an ability to add new task
+    if (task.trim().length === 0) {
+      setError('Task is required');
+      return;
+    }
     setTodos([...todos, { id: todos.length, label: task, checked: false }]);
+    setTask('');
+    setError('');
   };
 
   const handleKeyUp = (e) => {
@@ -17,17 +24,29 @@ export const TodoForm = () => {
     }
   };
 
+  React.useEffect(() => {
+    if (task.trim().length > 0) {
+      setError('');
+    }
+  }, [task]);
+
   return (
-    <div className="todo-form">
-      <input
-        placeholder="Enter new task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-        onKeyUp={handleKeyUp}
-      />
-      <button type="button" onClick={handleAddTodo}>
-        Add task
-      </button>
-    </div>
+    <>
+      <div className="todo-form">
+        <div>
+          <input
+            placeholder="Enter new task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            onKeyUp={handleKeyUp}
+            className={`${error && 'error-input'}`}
+          />
+          {error && <div className="error-text">{error}</div>}
+        </div>
+        <button type="button" onClick={handleAddTodo}>
+          Add task
+        </button>
+      </div>
+    </>
   );
 };
