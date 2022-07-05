51f029467078e268fa6a1a2c13c2188a285f684c
dark-king-rayleigh <ayushbaral17@gmail.com>
2022-07-05 17:29:25 +0545

    feat(todo):calculate total todos

--- src/components/todo-results/todo-results.jsx
+++ src/components/todo-results/todo-results.jsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import './todo-results.scss';
+import { TodosContext } from '../../todo-context';
 
 export const TodoResults = () => {
-  const calculateChecked = () => {
+  const { todos } = React.useContext(TodosContext);
+
+  const calculateChecked = React.useCallback(() => {
     // Fix an ability to calculate completed tasks
-  };
+    const completedTasks = todos.filter((todo) => todo.checked);
+    return completedTasks.length;
+  }, [todos]);
+
+  React.useEffect(() => {
+    calculateChecked();
+  }, [calculateChecked]);
 
   return (
     <div className="todo-results">
